c750871aee0795186d051f7c763cedcf633b4c48
Mvrouvne <marouanechaibat@gmail.com>
2025-07-17 15:56:47 +0100

    new things added

--- WorkflowAgents/ReasearchAssistant/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-query = "what is LLM?"
-url = "https://api.semanticscholar.org/graph/v1/paper/search"
-params = {"query": query}
-headers = {"x-api-key": "1234567890"}
-response = requests.get(url, params=params, headers=headers)
-print(response.json())
